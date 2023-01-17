80ca90234207b60eaddc9338d693376d0bd76a18
d0422 <rlfehd2013@naver.com>
2023-01-17 21:32:01 +0900

    refactor:컴포넌트 리팩토링

---
+++ src/components/MajorSection.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FusionType, IFusion } from '../type/subject';
+import CheckBox from './CheckBox';
+import { useRecoilValue } from 'recoil';
+import { CyberSecurity } from '../atom/TotalCredit';
+
+const MajorSection = ({ data }: { data: IFusion[] }) => {
+  const cyber = useRecoilValue(CyberSecurity);
+
+  function getDisplayType(type: FusionType) {
+    if (type === '전공기초') {
+      return cyber.majorBase;
+    }
+    if (type === '전공필수') {
+      return cyber.majorRequire;
+    }
+    if (type === '관계학') {
+      return cyber.relation;
+    }
+  }
+
+  return (
+    <>
+      <SubTitle>{data[0].type}</SubTitle>
+      <DataDisplay>{getDisplayType(data[0].type)} 학점 남았습니다.</DataDisplay>
+      {data.map((data: IFusion) => (
+        <CheckBox key={data.name} subjectInfo={data}></CheckBox>
+      ))}
+      <hr></hr>
+    </>
+  );
+};
+
+const SubTitle = styled.div`
+  justify-content: center;
+  font-size: 15px;
+`;
+const DataDisplay = styled.div`
+  font-size: 15px;
+  padding: 10px;
+`;
+export default MajorSection;
--- src/type/subject.ts
+++ src/type/subject.ts
@@ -1,5 +1,5 @@
-type subjectType = '전공' | '전공필수' | '전공기초';
-type FusionType = '전공기초' | '전공필수' | '관계학';
+export type subjectType = '전공' | '전공필수' | '전공기초';
+export type FusionType = '전공기초' | '전공필수' | '관계학';
 export interface ISubject {
   name: string;
   credit: number;
