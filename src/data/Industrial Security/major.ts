6217fb79795207e608b67d709b75fb232674ae19
d0422 <rlfehd2013@naver.com>
2023-01-18 17:58:55 +0900

    feat:산업보안학과 전공 데이터 추가

--- src/data/Industrial Security/major.ts
+++ src/data/Industrial Security/major.ts
@@ -38,5 +38,30 @@ const majorSubSet: ISubject[] = [
     credit: 3,
     hasTaken: false,
   },
+  {
+    name: '사이버 형사법',
+    type: '전공',
+    credit: 3,
+    hasTaken: false,
+  },
+  {
+    name: '산업보안심리',
+    type: '전공',
+    credit: 3,
+    hasTaken: false,
+  },
+  { name: '산업보안 조사와 포렌식', type: '전공', credit: 3, hasTaken: false },
+  {
+    name: '미래융합보안과 서비스(캡)',
+    type: '전공',
+    credit: 3,
+    hasTaken: false,
+  },
+  {
+    name: '산업융합보안1',
+    type: '전공',
+    credit: 3,
+    hasTaken: false,
+  },
 ];
 export default majorSubSet;
