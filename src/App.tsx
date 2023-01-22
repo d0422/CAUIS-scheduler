880c62a06502aba93ad2dfe7614d05826d0b6940
d0422 <rlfehd2013@naver.com>
2023-01-22 22:00:49 +0900

    feat:구글 애널리틱스 사용을 위한 react-ga4 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import Main from './page/Main';
 import { RecoilRoot } from 'recoil';
+import RouteChangeTracker from './components/RouteChangeTracker';
 function App() {
   return (
     <>
       <RecoilRoot>
         <GlobalStyle />
         <BrowserRouter>
+          <RouteChangeTracker />
           <Routes>
             <Route path="/" element={<Main />}></Route>
           </Routes>
